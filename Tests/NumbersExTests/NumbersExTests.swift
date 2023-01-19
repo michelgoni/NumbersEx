054c603e102d9e9a88da846b5f2f39bcac63e13d
Michel Goñi <mgoni@wabiproject.com>
2023-01-19 12:54:46 +0100

    Added Publisher extension + test

--- Tests/NumbersExTests/NumbersExTests.swift
+++ Tests/NumbersExTests/NumbersExTests.swift
@@ -1,7 +1,11 @@
 import XCTest
+import Combine
 @testable import NumbersEx
-
+@available(iOS 13.0, *)
 final class NumbersExTests: XCTestCase {
+
+    lazy var cancellables = Set<AnyCancellable>()
+
     func testIntExtension() throws {
         XCTAssertTrue(2.isPrime)
         XCTAssertFalse(4.isPrime)
@@ -21,9 +25,41 @@ final class NumbersExTests: XCTestCase {
         XCTAssertFalse(value.isEmpty)
     }
 
+    func testJust() throws {
+        let mockClass = MockClass()
+        mockClass.returnValue = .just("test")
+        mockClass.returnValue.sink { _ in
+
+        } receiveValue: { value in
+            XCTAssertTrue(value == "test")
+        }.store(in: &cancellables)
+    }
+
+    func testError() throws {
+        let mockClass = MockClass()
+        let expectation = expectation(description: "Extepcation for testError")
+        mockClass.returnValue = .fail(NSError())
+        mockClass.returnValue.sink { completion in
+            if case .failure = completion {
+                expectation.fulfill()
+            }
+        } receiveValue: { _ in
+
+        }.store(in: &cancellables)
+
+        wait(for: [expectation], timeout: 1.0)
+
+    }
+
 
     private func mockData(_ number: Int) async  -> Data {
 
         Data()
     }
+}
+
+@available(iOS 13.0, *)
+class MockClass {
+
+    var returnValue: AnyPublisher<String, Error>!
 }
