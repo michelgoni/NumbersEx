c852291030951f6909b07aaf2be9afb6264ad321
Michel Goñi <mgoni@wabiproject.com>
2023-02-03 12:19:15 +0100

    new extension for AsyncMap

--- Sources/NumbersEx/Extensions/Sequence+Extensions.swift
+++ Sources/NumbersEx/Extensions/Sequence+Extensions.swift
@@ -5,6 +5,7 @@
 //  Created by Michel Goñi on 19/1/23.
 //
 
+import Combine
 import Foundation
 
 extension Sequence {
@@ -18,5 +19,28 @@ extension Sequence {
         }
 
         return values
+    }
+
+
+}
+
+
+@available(iOS 13.0, *)
+extension Publisher {
+    func asyncMap<T>(
+        _ transform: @escaping (Output) async throws -> T
+    ) -> Publishers.FlatMap<Future<T, Error>, Self> {
+        flatMap { value in
+            Future { promise in
+                Task {
+                    do {
+                        let output = try await transform(value)
+                        promise(.success(output))
+                    } catch {
+                        promise(.failure(error))
+                    }
+                }
+            }
+        }
     }
 }
