a3dac80de37e39a896618f5163f73db236e86358
Michel <mgoni@mlean.com>
2023-08-21 17:39:31 +0200

    Added more associated types

--- Sources/NumbersEx/Viewmodel/ViewmodelObservable.swift
+++ Sources/NumbersEx/Viewmodel/ViewmodelObservable.swift
@@ -10,5 +10,9 @@ import SwiftUI
 
 public protocol ViewModelObservable: ObservableObject {
     associatedtype Input
+    associatedtype Datasource: Equatable
+    associatedtype Destination: Equatable
+    
     func trigger(_ input: Input)
+    
 }
