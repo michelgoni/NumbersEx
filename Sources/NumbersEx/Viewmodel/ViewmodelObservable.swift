adde6e0cc2758e5027dc49ea119140297c2af8f3
Michel <mgoni@mlean.com>
2023-08-21 17:48:26 +0200

    Removed datasource

--- Sources/NumbersEx/Viewmodel/ViewmodelObservable.swift
+++ Sources/NumbersEx/Viewmodel/ViewmodelObservable.swift
@@ -13,8 +13,6 @@ public protocol ViewModelObservable: ObservableObject {
     associatedtype Datasource: Equatable
     associatedtype Destination: Equatable
     
-    var dataSource: Datasource { get }
-    
     func trigger(_ input: Input)
     
 }
